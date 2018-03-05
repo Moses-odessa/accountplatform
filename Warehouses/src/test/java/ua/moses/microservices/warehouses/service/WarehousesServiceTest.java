72c803e37ef04aee48002fb1ea35a9a2c5a961b6
Vladimir Zadorozhny <moses2804@gmail.com>
2018-03-05 15:06:03 +0200

    add getWarehouseById and fix some typo

--- Warehouses/src/test/java/ua/moses/microservices/warehouses/service/WarehousesServiceTest.java
+++ Warehouses/src/test/java/ua/moses/microservices/warehouses/service/WarehousesServiceTest.java
@@ -13,7 +13,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
