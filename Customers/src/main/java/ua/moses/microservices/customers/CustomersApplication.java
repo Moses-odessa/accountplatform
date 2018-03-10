692bfd4564d1b512afe5c31d28ce6bed5070287b
Vladimir Zadorozhny <moses2804@gmail.com>
2018-03-10 10:27:48 +0200

    Customers - add eureka

--- Customers/src/main/java/ua/moses/microservices/customers/CustomersApplication.java
+++ Customers/src/main/java/ua/moses/microservices/customers/CustomersApplication.java
@@ -2,7 +2,9 @@ package ua.moses.microservices.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CustomersApplication {
     public static void main(String[] args) {
