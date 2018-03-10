a916f8734d4bfdddaf73fd90f4179e08682a5829
Vladimir Zadorozhny <moses2804@gmail.com>
2018-03-10 10:21:20 +0200

    Companies - add eureka

--- Companies/src/main/java/ua/moses/microservices/companies/CompaniesApplication.java
+++ Companies/src/main/java/ua/moses/microservices/companies/CompaniesApplication.java
@@ -2,7 +2,9 @@ package ua.moses.microservices.companies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class CompaniesApplication {
     public static void main(String[] args) {
