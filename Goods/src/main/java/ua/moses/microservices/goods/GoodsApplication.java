19aeae9dedf510b66772a7768f21b955c078c29a
Vladimir Zadorozhny <moses2804@gmail.com>
2018-03-10 10:15:57 +0200

    Goods - add eureka

--- Goods/src/main/java/ua/moses/microservices/goods/GoodsApplication.java
+++ Goods/src/main/java/ua/moses/microservices/goods/GoodsApplication.java
@@ -2,7 +2,9 @@ package ua.moses.microservices.goods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class GoodsApplication {
     public static void main(String[] args) {
